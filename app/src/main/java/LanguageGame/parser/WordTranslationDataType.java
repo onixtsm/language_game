b9d454b18d10863ca622e8cee2c475448d399291
onixtsm <alexander@sautkin.com>
2021-04-01 17:17:28 +0300

    Has to work fine. DK how to launch correctly

--- app/src/main/java/LanguageGame/parser/WordTranslationDataType.java
+++ app/src/main/java/LanguageGame/parser/WordTranslationDataType.java
@@ -1,14 +1,16 @@
 package LanguageGame.parser;
 
 import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class WordTranslationDataType {
 
         private String sourceLanguage;
-        private List<String> translationLanguage;
-        private List<String> wordType;
+        private List<String> translationLanguage = new ArrayList<>();
+        private List<String> wordType = new ArrayList<>();
 
         public void setTranslationLanguage(String s) {
                 System.out.println(this);
